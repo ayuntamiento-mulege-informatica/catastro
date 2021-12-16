ab76086250efb57f5e0681f2517748073fa1d747
Menoru <menoru.da@gmail.com>
2021-12-16 08:47:50 -0700

    Primer commit.

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>SISTEMA PARA CATASTRO</title>
+  <link rel="stylesheet" href="/css/fontawesome.css">
+  <link rel="stylesheet" href="/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+  <header>
+    <h1>SISTEMA PARA CATASTRO</h1>
+  </header>
---
+++ inicio_informatica.php
@@ -0,0 +1,40 @@
+<?php
+include_once 'header.php';
+include_once 'menu.php';
+?>
+
+<main class="container">
+  <section class="row">
+    <div class="col-12 centrar-contenedor">
+      <div class="contenedor">
+        <div class="titulo-contenedor">
+          <h2>Informática</h2>
+        </div>
+
+        <div class="contenido-contenedor">
+          <div class="container-fluid">
+            <div class="row justify-content-center">
+              <div class="col-4">
+                <h3>Lorem ipsum</h3>
+                <div class="centrar-botones">
+                  <a class="enlace-boton" href="/">Lorem ipsum</a>
+                  <a class="enlace-boton" href="/">Lorem ipsum</a>
+                  <a class="enlace-boton" href="/">Lorem ipsum</a>
+                </div>
+              </div>
+
+              <div class="col-4">
+                <h3>Tickets</h3>
+                <div class="centrar-botones">
+                  <a class="enlace-boton" href="/revisar_tickets">Revisar tickets</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php include_once 'footer.php'; ?>
